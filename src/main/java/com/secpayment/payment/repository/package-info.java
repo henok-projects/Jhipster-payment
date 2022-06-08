4b1002518f697145f2b32a2b20ace3d5b856fe35
Henok <henokaddis72@gmail.com>
2022-06-08 13:01:22 +0300

    Initial version of payment generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/secpayment/payment/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data JPA repositories.
+ */
+package com.secpayment.payment.repository;
---
+++ src/test/java/com/secpayment/payment/config/NoOpMailConfiguration.java
@@ -0,0 +1,25 @@
+package com.secpayment.payment.config;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+
+import com.secpayment.payment.service.MailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NoOpMailConfiguration {
+
+    private final MailService mockMailService;
+
+    public NoOpMailConfiguration() {
+        mockMailService = mock(MailService.class);
+        doNothing().when(mockMailService).sendActivationEmail(any());
+    }
+
+    @Bean
+    public MailService mailService() {
+        return mockMailService;
+    }
+}
